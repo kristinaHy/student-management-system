99706c24a316f8e0215e98bd4006ce30583a7b80
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-08 12:47:05 +0545

    Add Main class to test student CRUD operations

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,36 @@
-import database.DBConnection;
+import dao.StudentDAO;
+import model.Student;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DBConnection.getConnection();
+        StudentDAO dao = new StudentDAO();
 
+        // CREATE
+        Student student = new Student(
+                "Kristina",
+                "Female",
+                "kristina@email.com",
+                "9800000000",
+                "photo.jpg"
+        );
+
+        dao.addStudent(student);
+
+        // READ
+        List<Student> students = dao.getAllStudents();
+
+        for (Student s : students) {
+            System.out.println(
+                    s.getId() + " | " +
+                            s.getName() + " | " +
+                            s.getGender() + " | " +
+                            s.getEmail() + " | " +
+                            s.getPhone()
+            );
+        }
     }
-
 }
