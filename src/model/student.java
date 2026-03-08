d2cf0149023844072514e9eae649fd0fc9ae0b61
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-08 11:52:18 +0545

    feat: add Student model class

--- src/model/student.java
+++ src/model/student.java
@@ -4,18 +4,34 @@ public class Student {
 
     private int id;
     private String name;
+    private String gender;
     private String email;
     private String phone;
 
-    public Student(int id, String name, String email, String phone) {
+    public Student() {}
+
+    public Student(int id, String name, String gender, String email, String phone) {
         this.id = id;
         this.name = name;
+        this.gender = gender;
         this.email = email;
         this.phone = phone;
     }
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public String getPhone() { return phone; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
