bc3347de9553906ed978742ebd32d1d1be65cfe6
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-08 13:25:19 +0545

    Changed model student.java

---
+++ src/controller/StudentController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class StudentController {
+
+    public void saveStudent(String name, String email) {
+
+        System.out.println("Saving student...");
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+
+    }
+
+}
