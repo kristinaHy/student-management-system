45b5f6664c4547dd949e6a56838718c2d5e7d670
Kristina Hyaunmkiha <hyaunmikhakristina@gmail.com>
2026-03-08 11:45:20 +0545

    Add Student model class

---
+++ src/model/student.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Student(int id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+}
